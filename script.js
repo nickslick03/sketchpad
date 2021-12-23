177e0a82bc1b5099c15026311a3018fa6472e22a
nickslick03 <nicholasepps03@gmail.com>
2021-12-23 07:00:48 -0500

    setColumns function

--- script.js
+++ script.js
@@ -1,27 +1,74 @@
-const container = document.getElementById("sketchpad-container");
-const grid = [];
-let squares = 900;
-let columns = Math.sqrt(squares);
-
-for(let i=0;i<squares;i++)
-{
-    grid[i] = document.createElement('div');
-    grid[i].classList.add('grid-item');
-    grid[i].addEventListener('mousedown',function(){grid[i].classList.add('black');});
-    grid[i].addEventListener('mouseover', function(event)
-    {
-        if(event.buttons==1)
-        {
-            grid[i].classList.add('black');
-            console.log(event.buttons);
-        }
-    });
-    container.appendChild(grid[i]); 
-}
+const sketchpadContainer = document.getElementById("sketchpad-container");
+const style = document.createElement("style");
+const body = document.querySelector("body");
+let grid = [];
+let columns = 30;
+let squares = columns*columns;
+let color = "black";
 
 let auto = "auto ";
 for(let i=1;i<columns;i++)
 {
     auto+="auto ";
 }
-container.style.cssText=`grid-template-columns:${auto};`;
+sketchpadContainer.style.cssText=`grid-template-columns:${auto};`;
+
+for(let i=0;i<squares;i++)
+{
+    grid[i] = document.createElement('div');
+    grid[i].classList.add('grid-item');
+    
+    grid[i].addEventListener('mousedown',function(){grid[i].style.cssText=`background-color:${color}`;});
+    grid[i].addEventListener('mouseover', function(event)
+    {
+        if(event.buttons==1)
+        {
+            grid[i].style.cssText=`background-color:${color}`;
+        }
+    });
+    sketchpadContainer.appendChild(grid[i]); 
+}
+style.innerHTML = `.grid-item:hover{background-color:${color}}`;
+body.appendChild(style);
+
+function setColumns()
+{
+    if(grid.length>0)
+    for(let i=0;i<squares;i++)
+    {
+        sketchpadContainer.removeChild(grid[i]); 
+    }
+    grid = [];
+    columns = document.getElementById("columns").value;
+    squares = columns*columns;
+    let auto = "auto ";
+    for(let i=1;i<columns;i++)
+    {
+        auto+="auto ";
+    }
+    sketchpadContainer.style.cssText=`grid-template-columns:${auto};`;
+    
+    for(let i=0;i<squares;i++)
+    {
+        grid[i] = document.createElement('div');
+        grid[i].classList.add('grid-item');
+            
+        grid[i].addEventListener('mousedown',function(){grid[i].style.cssText=`background-color:${color}`;});
+        grid[i].addEventListener('mouseover', function(event)
+        {
+            if(event.buttons==1)
+            {
+                grid[i].style.cssText=`background-color:${color}`;
+            }
+        });
+        sketchpadContainer.appendChild(grid[i]);
+    }
+    style.innerHTML = `.grid-item:hover{background-color:${color}}`;
+    body.appendChild(style);
+}
+
+function reset()
+{
+    location.reload();
+}
+
