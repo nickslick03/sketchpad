fd7d75d876352424a05a8c28d0e5416b6d7ef5a6
nickslick03 <nicholasepps03@gmail.com>
2021-12-24 09:03:12 -0500

    changeColor function

--- script.js
+++ script.js
@@ -2,34 +2,9 @@ const sketchpadContainer = document.getElementById("sketchpad-container");
 const style = document.createElement("style");
 const body = document.querySelector("body");
 let grid = [];
-let columns = 30;
+let columns = 25;
 let squares = columns*columns;
 let color = "black";
-
-let auto = "auto ";
-for(let i=1;i<columns;i++)
-{
-    auto+="auto ";
-}
-sketchpadContainer.style.cssText=`grid-template-columns:${auto};`;
-
-for(let i=0;i<squares;i++)
-{
-    grid[i] = document.createElement('div');
-    grid[i].classList.add('grid-item');
-    
-    grid[i].addEventListener('mousedown',function(){grid[i].style.cssText=`background-color:${color}`;});
-    grid[i].addEventListener('mouseover', function(event)
-    {
-        if(event.buttons==1)
-        {
-            grid[i].style.cssText=`background-color:${color}`;
-        }
-    });
-    sketchpadContainer.appendChild(grid[i]); 
-}
-style.innerHTML = `.grid-item:hover{background-color:${color}}`;
-body.appendChild(style);
 
 function setColumns()
 {
@@ -39,8 +14,14 @@ function setColumns()
         sketchpadContainer.removeChild(grid[i]); 
     }
     grid = [];
+    
     columns = document.getElementById("columns").value;
+    if(columns === '')
+    {
+        columns = 25;
+    }
     squares = columns*columns;
+
     let auto = "auto ";
     for(let i=1;i<columns;i++)
     {
@@ -53,7 +34,10 @@ function setColumns()
         grid[i] = document.createElement('div');
         grid[i].classList.add('grid-item');
             
-        grid[i].addEventListener('mousedown',function(){grid[i].style.cssText=`background-color:${color}`;});
+        grid[i].addEventListener('mousedown',function()
+        {
+            grid[i].style.cssText=`background-color:${color}`;
+        });
         grid[i].addEventListener('mouseover', function(event)
         {
             if(event.buttons==1)
@@ -66,6 +50,38 @@ function setColumns()
     style.innerHTML = `.grid-item:hover{background-color:${color}}`;
     body.appendChild(style);
 }
+
+function changeColor()
+{
+    for(let i=0;i<squares;i++)
+    {
+        grid[i].addEventListener('mousedown',function()
+        {
+            grid[i].style.cssText=`background-color:${color}`;
+        });
+        grid[i].addEventListener('mouseover', function(event)
+        {
+            if(event.buttons==1)
+            {
+                grid[i].style.cssText=`background-color:${color}`;
+            }
+        });
+    }
+    style.innerHTML = `.grid-item:hover{background-color:${color}}`;
+}
+
+var colors = document.getElementById('color-container').childNodes;
+
+colors.forEach(function(e)
+    {
+        e.addEventListener('click', function()
+        {
+            color = e.id;
+            changeColor();
+        });
+    });
+
+setColumns();
 
 function reset()
 {
